882b2f137d014b1282c5487b66610573677f3b19
eghadirian <60559670+eghadirian@users.noreply.github.com>
2021-07-03 17:34:13 -0400

    Update the keras pipeline

--- P5-SignalProcessing.py
+++ P5-SignalProcessing.py
@@ -18,20 +18,20 @@ from tensorflow import keras
 from keras.layers import Dense, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.models import Sequential
-from keras.callbacks import History 
+from keras.callbacks import History
 
 activities = ['WALKING', 'WALKING_UPSTAIRS', \
               'WALKING_DOWNSTAIRS', 'SITTING', \
               'STANDING', 'LAYING']
 train_signals, test_signals = [], []
 for input_file in os.listdir('UCI HAR Dataset/train/Inertial Signals/'):
-        signal = pd.read_csv('UCI HAR Dataset/train/Inertial Signals/'+input_file, delim_whitespace=True, header=None)
-        train_signals.append(signal)
+  signal = pd.read_csv('UCI HAR Dataset/train/Inertial Signals/'+input_file, delim_whitespace=True, header=None)
+  train_signals.append(signal)
 train_signals = np.transpose(np.array(train_signals), (1, 2, 0))
 y_train = pd.read_csv('UCI HAR Dataset/train/y_train.txt', header=None)
 for input_file in os.listdir('UCI HAR Dataset/test/Inertial Signals/'):
-        signal = pd.read_csv('UCI HAR Dataset/test/Inertial Signals/'+input_file, delim_whitespace=True, header=None)
-        test_signals.append(signal)
+  signal = pd.read_csv('UCI HAR Dataset/test/Inertial Signals/'+input_file, delim_whitespace=True, header=None)
+  test_signals.append(signal)
 test_signals = np.transpose(np.array(test_signals), (1, 2, 0))
 y_test = pd.read_csv('UCI HAR Dataset/test/y_test.txt', header=None)
 
@@ -72,28 +72,27 @@ test_size= np.shape(test_signals)[0]
 n_comp = np.shape(train_signals)[2]
 train_data_cwt = np.ndarray(shape=(train_size, N-1, N-1, n_comp))
 for ii in range(0,train_size):
-    if ii % 1000 == 0:
-        print(ii)
-    for jj in range(0,n_comp):
-        signal = train_signals[ii, :, jj]
-        coeff, freq = pywt.cwt(signal, scales, waveletname, 1)
-        coeff_ = coeff[:,:N-1]
-        train_data_cwt[ii, :, :, jj] = coeff_
+  if ii % 1000 == 0:
+    print(ii)
+  for jj in range(0,n_comp):
+    signal = train_signals[ii, :, jj]
+    coeff, freq = pywt.cwt(signal, scales, waveletname, 1)
+    coeff_ = coeff[:,:N-1]
+    train_data_cwt[ii, :, :, jj] = coeff_
 test_data_cwt = np.ndarray(shape=(test_size, N-1, N-1, n_comp))
 for ii in range(0,test_size):
-    if ii % 1000 == 0:
-        print(ii)
-    for jj in range(0,n_comp):
-        signal = test_signals[ii, :, jj]
-        coeff, freq = pywt.cwt(signal, scales, waveletname, 1)
-        coeff_ = coeff[:,:N-1]
-        test_data_cwt[ii, :, :, jj] = coeff_
-y_train = list(map(lambda x: int(x) - 1, y_train))
-y_test = list(map(lambda x: int(x) - 1, y_test))
+  if ii % 1000 == 0:
+    print(ii)
+  for jj in range(0,n_comp):
+    signal = test_signals[ii, :, jj]
+    coeff, freq = pywt.cwt(signal, scales, waveletname, 1)
+    coeff_ = coeff[:,:N-1]
+    test_data_cwt[ii, :, :, jj] = coeff_
+
 x_train = train_data_cwt
-y_train = list(y_train[:train_size])
 x_test = test_data_cwt
-y_test = list(y_test[:test_size])
+y_train = y_train[:train_size]
+y_test = y_test[:test_size]
 
 history = History()
 img_x = N-1
@@ -101,29 +100,32 @@ img_y = N-1
 img_z = n_comp
 input_shape = (img_x, img_y, img_z)
 batch_size = 16
-num_classes = 7
+num_classes = np.unique(y_train)[-1]+1
 epochs = 10
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
-model = Sequential(
+
+model = Sequential([
     Conv2D(32, kernel_size=(5, 5), strides=(1, 1), activation='relu', input_shape=input_shape),
     MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
     Conv2D(64, (5, 5), activation='relu'),
     MaxPooling2D(pool_size=(2, 2)),
     Flatten(),
+    BatchNormalization(),
     Dense(1000, activation='relu'),
+    BatchNormalization(),
     Dense(num_classes, activation='softmax')
-)
+])
 model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adam(),
               metrics=['accuracy'])
 model.fit(x_train, y_train,
           batch_size=batch_size,
           epochs=epochs,
-          verbose=1,
-          validation_data=(x_test, y_test),
+          verbose = 1,
+          validation_split = 0.1,
           callbacks=[history])
 train_score = model.evaluate(x_train, y_train, verbose=0)
 print('Train loss: {}, Train accuracy: {}'.format(train_score[0], train_score[1]))
