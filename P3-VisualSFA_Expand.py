8208967980aa7092550cd76f3b3ab1db0cdaf13e
eghadirian <60559670+eghadirian@users.noreply.github.com>
2021-07-08 13:30:59 -0400

    add r2

--- P3-VisualSFA_Expand.py
+++ P3-VisualSFA_Expand.py
@@ -2,6 +2,10 @@ import numpy as np
 from matplotlib import pyplot as plt
 import mdp
 from scipy.ndimage import gaussian_filter
+from sklearn.metrics import r2_score
+
+def normalize(X):
+  return (X-np.mean(X))/np.std(X)
 
 np.random.seed(42)
 series_length = 512
@@ -35,19 +39,21 @@ plt.plot(x2,x3)
 
 plt.figure()
 plt.subplot(1,3,1)
-plt.plot(t,a1)
+plt.plot(t,normalize(a1))
 plt.subplot(1,3,2)
-plt.plot(t,a2)
+plt.plot(t,normalize(a2))
 plt.subplot(1,3,3)
-plt.plot(t,a5)
+plt.plot(t,normalize(a5))
 
-
+output_dim = 3
+time_embd = 2
+poly_exp = 2
 flow = (
          mdp.nodes.EtaComputerNode() +
-         mdp.nodes.PCANode(output_dim=3) +
-         mdp.nodes.TimeFramesNode(2) +
-         mdp.nodes.PolynomialExpansionNode(2) +
-         mdp.nodes.SFANode(output_dim=3) +
+         mdp.nodes.PCANode(output_dim=output_dim) +
+         mdp.nodes.TimeFramesNode(time_embd) +
+         mdp.nodes.PolynomialExpansionNode(poly_exp) +
+         mdp.nodes.SFANode(output_dim=output_dim) +
          mdp.nodes.EtaComputerNode()
          )
 
@@ -56,16 +62,17 @@ slow = flow(x)
 
 plt.figure()
 plt.subplot(1,3,1)
-plt.plot(np.arange(series_length-1)*4*np.pi/512, slow[:,0], \
+plt.plot(np.arange(series_length+1-time_embd)*4*np.pi/512, slow[:,0], \
     label = '{:4.2f}'.format(flow[-1].get_eta(t=series_length)[0]))
 plt.legend()
 plt.subplot(1,3,2)
-plt.plot(np.arange(series_length-1)*4*np.pi/512, slow[:,1], \
+plt.plot(np.arange(series_length+1-time_embd)*4*np.pi/512, slow[:,1], \
     label = '{:4.2f}'.format(flow[-1].get_eta(t=series_length)[1]))
 plt.legend()
 plt.subplot(1,3,3)
-plt.plot(np.arange(series_length-1)*4*np.pi/512, slow[:,2], \
+plt.plot(np.arange(series_length+1-time_embd)*4*np.pi/512, slow[:,2], \
     label = '{:4.2f}'.format(flow[-1].get_eta(t=series_length)[2]))
 plt.legend()
 
+print('r2: {:4.3}'.format(r2_score(slow[:,0], normalize(a5[:1-time_embd]))))
 plt.show()
